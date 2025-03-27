48625c2258c7db2a982925ee2138b188b5a86aa6
Nazar <nazarchernivchan9002@gmail.com>
2025-03-27 18:20:06 +0100

    Add and display assets

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
+type Asset = { name: string; balance: number };
+
 const Content: React.FC = () => {
-  return <div style={{ background: "red" }}>Content</div>;
+  const [assetName, setAssetName] = useState<string>("");
+  const [assetBalance, setAssetBalance] = useState<number>(0);
+
+  const [assets, setAssets] = useState<Asset[]>([]);
+  return (
+    <div>
+      <section>
+        <label htmlFor={"asset_name"}>Asset name</label>
+        <input
+          id={"asset_name"}
+          type={"text"}
+          value={assetName}
+          onChange={(event) => setAssetName(event.target.value)}
+        />
+        <input
+          id={"asset_balance"}
+          type={"number"}
+          value={assetBalance}
+          onChange={(event) => {
+            setAssetBalance(parseInt(event.target.value));
+          }}
+        />
+        <button
+          onClick={() =>
+            setAssets(assets.concat({ name: assetName, balance: assetBalance }))
+          }
+        >
+          Add Asset
+        </button>
+      </section>
+      <div>
+        {assets.map((asset) => (
+          <div>
+            asset:{asset.name}
+            <br /> balance: {asset.balance}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Content;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,49 @@
 const Header: React.FC = () => {
-  return <div style={{ background: "orange" }}>Beweathly</div>;
+  return (
+    <div
+      style={{
+        background: "#347deb",
+        fontSize: "27px",
+        padding: "1.7%",
+        width: "96.6%",
+        fontFamily: "arial",
+      }}
+    >
+      <span>Bewealthy</span>
+      <button
+        style={{
+          color: "white",
+          border: "1px solid black",
+          borderRadius: "5px",
+          padding: "0.5%",
+          background: "#347deb",
+          float: "right",
+          fontFamily: "arial",
+          fontSize: "18px",
+          cursor: "pointer",
+          marginRight: "1%",
+        }}
+      >
+        Sign up
+      </button>
+      <button
+        style={{
+          color: "white",
+          border: "1px solid black",
+          borderRadius: "5px",
+          padding: "0.5%",
+          background: "#347deb",
+          float: "right",
+          fontFamily: "arial",
+          fontSize: "18px",
+          cursor: "pointer",
+          marginRight: "1%",
+        }}
+      >
+        Sign in
+      </button>
+    </div>
+  );
 };
 
 export default Header;
