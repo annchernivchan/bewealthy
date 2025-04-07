d085faedf84b0ad4d935d4649e21e8d1202496cf
Mykola Khytra <mykola.khytra@shelf.io>
2025-04-07 16:53:19 +0200

    feat: BW-18 refactor AddAsset.tsx

--- src/components/AddAsset.tsx
+++ src/components/AddAsset.tsx
@@ -1,45 +1,50 @@
-import { useState } from "react";
-import { Asset } from "./Content";
+import { useState } from 'react';
+import { Asset } from './Content';
+import * as React from 'react';
 
 type AddAssetProps = { onAdd: (asset: Asset) => void };
 
 const AddAsset: React.FC<AddAssetProps> = ({ onAdd }) => {
-  const [assetName, setAssetName] = useState<string>("");
+  const [assetName, setAssetName] = useState<string>('');
   const [assetBalance, setAssetBalance] = useState<number>(0);
 
   return (
     <section>
-      <label htmlFor={"asset_name"}>Asset name</label>
-      <input
-        id={"asset_name"}
-        type={"text"}
-        value={assetName}
-        onChange={(event) => setAssetName(event.target.value)}
-      />
-      <label htmlFor={"asset_balance"}>Balance</label>
-      <input
-        min="0"
-        id={"asset_balance"}
-        type={"number"}
-        value={assetBalance}
-        onChange={(event) => {
-          setAssetBalance(parseInt(event.target.value));
-        }}
-      />
-      <button
-        disabled={assetName === "" || assetBalance < 0}
-        onClick={() => {
-          onAdd({
-            name: assetName,
-            balance: assetBalance,
-            id: Math.random().toString(36).substring(2, 10),
-          });
-          setAssetName("");
-          setAssetBalance(0);
-        }}
-      >
-        Add Asset
-      </button>
+      <div style={{ display: 'flex', alignItems: 'flex-start', gap: '16px' }}>
+        <div>
+          <label htmlFor={'asset_name'}>Asset name</label>
+          <input
+            id={'asset_name'}
+            type={'text'}
+            value={assetName}
+            onChange={(event) => setAssetName(event.target.value)}
+          />
+          <label htmlFor={'asset_balance'}>Balance</label>
+          <input
+            min="0"
+            id={'asset_balance'}
+            type={'number'}
+            value={assetBalance}
+            onChange={(event) => {
+              setAssetBalance(parseInt(event.target.value));
+            }}
+          />
+          <button
+            disabled={assetName === '' || assetBalance < 0}
+            onClick={() => {
+              onAdd({
+                name: assetName,
+                balance: assetBalance,
+                id: Math.random().toString(36).substring(2, 10)
+              });
+              setAssetName('');
+              setAssetBalance(0);
+            }}
+          >
+            Add Asset
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
