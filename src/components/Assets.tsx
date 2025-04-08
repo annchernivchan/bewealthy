1c0990ee8b889054170af8c6a83c63b3a28bab0c
Nazar <nazarchernivchan9002@gmail.com>
2025-04-08 22:07:00 +0200

    feature: BW-20 Add editing assets

--- src/components/Assets.tsx
+++ src/components/Assets.tsx
@@ -1,17 +1,67 @@
 import Button from '@mui/material/Button';
 import { Asset } from './Content';
 import * as React from 'react';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
 
-type AssetsProps = { assets: Asset[]; onDelete: (id: string) => void };
+type AssetsProps = {
+  assets: Asset[];
+  onDelete: (id: string) => void;
+  onEdit: (asset: Asset) => void;
+};
 
-const Assets: React.FC<AssetsProps> = ({ assets, onDelete }) => {
+const Assets: React.FC<AssetsProps> = ({ assets, onDelete, onEdit }) => {
+  const [editingAssetId, setEditingAssetId] = useState<string | undefined>();
+
   return (
     <div>
       {assets.map(asset => (
         <div key={asset.id}>
-          asset: {asset.name}
-          <br /> balance: {asset.balance}
-          <br />
+          {editingAssetId === asset.id ? (
+            <div>
+              <TextField
+                label="Asset Name"
+                variant="standard"
+                defaultValue={asset.name}
+              />
+              <br />
+              <TextField
+                label="Balance"
+                type="number"
+                variant="standard"
+                defaultValue={asset.balance}
+              />
+              <Button
+                variant="contained"
+                color="success"
+                onClick={() => {
+                  onEdit({
+                    id: asset.id,
+                    name: asset.name,
+                    balance: asset.balance,
+                  });
+                  setEditingAssetId(undefined);
+                }}
+              >
+                Done
+              </Button>
+            </div>
+          ) : (
+            <div>
+              asset: {asset.name}
+              <br /> balance: {asset.balance}
+              <br />
+            </div>
+          )}
+          <Button
+            variant="outlined"
+            size="medium"
+            onClick={() => {
+              setEditingAssetId(asset.id);
+            }}
+          >
+            Edit
+          </Button>
           <Button
             onClick={() => onDelete(asset.id)}
             variant="outlined"
