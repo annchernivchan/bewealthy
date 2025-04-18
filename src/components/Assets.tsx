7aa07415687c21b78d09df44435a732e9b66e812
Nazar <nazarchernivchan9002@gmail.com>
2025-04-18 22:03:31 +0200

    feature: BW-24 Use Drawer for assets editing

--- src/components/Assets.tsx
+++ src/components/Assets.tsx
@@ -5,6 +5,13 @@ import { useState } from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditingAsset from './EditingAsset';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 type AssetsProps = {
   assets: Asset[];
@@ -15,43 +22,62 @@ type AssetsProps = {
 const Assets: React.FC<AssetsProps> = ({ assets, onDelete, onEdit }) => {
   const [editingAssetId, setEditingAssetId] = useState<string | undefined>();
   return (
-    <div>
-      {assets.map(asset => (
-        <div key={asset.id}>
-          {editingAssetId === asset.id ? (
-            <EditingAsset
-              asset={asset}
-              onEdit={asset => {
-                onEdit(asset);
-                setEditingAssetId(undefined);
-              }}
-            />
-          ) : (
-            <div>
-              asset: {asset.name}
-              <br /> balance: {asset.balance}
-              <br />
-            </div>
-          )}
-          <Button
-            variant="outlined"
-            size="medium"
-            onClick={() => {
-              setEditingAssetId(asset.id);
-            }}
-          >
-            <EditIcon />
-          </Button>
-          <Button
-            onClick={() => onDelete(asset.id)}
-            variant="outlined"
-            color="error"
-          >
-            <DeleteOutlineIcon />
-          </Button>
-        </div>
-      ))}
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Balance</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {assets.map(asset => (
+            <TableRow
+              key={asset.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {asset.name}
+              </TableCell>
+              <TableCell align="right">{asset.balance}</TableCell>
+              <TableCell align="right">
+                <Button
+                  variant="outlined"
+                  size="medium"
+                  onClick={() => {
+                    setEditingAssetId(asset.id);
+                  }}
+                >
+                  <EditIcon />
+                </Button>
+                <EditingAsset
+                  asset={asset}
+                  onEdit={asset => {
+                    onEdit(asset);
+                    setEditingAssetId(undefined);
+                  }}
+                  isOpen={asset.id === editingAssetId}
+                  onClose={() => {
+                    setEditingAssetId(undefined);
+                  }}
+                />
+              </TableCell>
+              <TableCell align="right">
+                <Button
+                  onClick={() => onDelete(asset.id)}
+                  variant="outlined"
+                  color="error"
+                >
+                  <DeleteOutlineIcon />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
