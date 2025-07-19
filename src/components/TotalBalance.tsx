34c5d050f2703fd5f4568d10d1272360ef349223
Nazar <nazarchernivchan9002@gmail.com>
2025-07-19 23:13:19 +0200

    BW-28: Save Currency in localStorage

--- src/components/TotalBalance.tsx
+++ src/components/TotalBalance.tsx
@@ -1,15 +1,21 @@
 import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import * as React from 'react';
 import { EUR, UAH, USD } from '../constants/CurrencyConstants';
+import { Currency } from '../types/types';
 
 type TotalBalanceProps = {
   balance: number;
+  selectedCurrency: Currency;
+  onCurrencyChange: (currency: Currency) => void;
 };
 
-const TotalBalance: React.FC<TotalBalanceProps> = ({ balance }) => {
+const TotalBalance: React.FC<TotalBalanceProps> = ({
+  balance,
+  selectedCurrency,
+  onCurrencyChange,
+}) => {
   return (
     <div
       style={{
@@ -38,13 +44,34 @@ const TotalBalance: React.FC<TotalBalanceProps> = ({ balance }) => {
         {balance}
         <FormControl sx={{ minWidth: 60 }} size="small">
           <Select
-            defaultValue={UAH}
+            defaultValue={selectedCurrency}
             labelId="currency-label"
             id="currency-select"
           >
-            <MenuItem value={UAH}>{UAH}</MenuItem>
-            <MenuItem value={USD}>{USD}</MenuItem>
-            <MenuItem value={EUR}>{EUR}</MenuItem>
+            <MenuItem
+              value={UAH}
+              onClick={() => {
+                onCurrencyChange(UAH);
+              }}
+            >
+              {UAH}
+            </MenuItem>
+            <MenuItem
+              value={USD}
+              onClick={() => {
+                onCurrencyChange(USD);
+              }}
+            >
+              {USD}
+            </MenuItem>
+            <MenuItem
+              value={EUR}
+              onClick={() => {
+                onCurrencyChange(EUR);
+              }}
+            >
+              {EUR}
+            </MenuItem>
           </Select>
         </FormControl>
       </div>
